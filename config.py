928fe0024b2f226b9f6b662eb97545445fd16cfe
Ivanenko_Bohdan <crasydoktor@gmail.com>
2025-05-26 17:12:32 +0300

    Додано посилання для новин

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
 STUDENT_ID = "Ivanenko_1a5867af"
-SOURCES = []
+SOURCES = [
+    "https://feeds.bbci.co.uk/news/rss.xml",            # BBC News
+    "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",  # New York Times
+    "https://www.theguardian.com/world/rss",            # The Guardian World News
+    "https://rss.cnn.com/rss/edition.rss",              # CNN Top Stories
+    "https://feeds.a.dj.com/rss/RSSWorldNews.xml",      # Wall Street Journal World News
+    "https://www.aljazeera.com/xml/rss/all.xml",        # Al Jazeera English
+    "https://www.reutersagency.com/feed/?best-topics=business-finance", # Reuters Business
+    "https://www.npr.org/rss/rss.php?id=1001",          # NPR News
+]
